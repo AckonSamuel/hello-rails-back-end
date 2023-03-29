353b2d5379fcdd814971f2a540f15f78c8df1f0f
Ackon Samuel <ackons433@gmail.com>
2023-03-29 15:03:36 +0000

    Set routes and seed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,6 @@
 class MessagesController < ApplicationController
   def index
-    @messages = Message.all
-    num = rand(1..@messages.size)
-    @message = Message.find(num)
+    @message = Message.first
     render json: @message, status: :ok
   end
 end
